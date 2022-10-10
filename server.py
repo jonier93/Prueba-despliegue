79b9ee8a44ca3921affb5a03f67feff7b69119fa
jonier93 <jonietrporras@gmail.com>
2022-10-10 13:11:08 -0500

    version2

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import baseDatos
 
-app = Flask(__name__, static_url_path='', static_folder='../frontend/static/', template_folder='../frontend/templates')
+app = Flask(__name__, static_url_path='', static_folder='frontend/static/', template_folder='frontend/templates')
 
 from routes.rutas import * #Debe ir después de declarar app, ya que app es usado en este módulo que se está importando
 
